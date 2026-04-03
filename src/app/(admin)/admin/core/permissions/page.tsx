db347ab412d3388db2233f9f0baba09ce2df98e1
hoangvm-vhv <hoangvm@vhv.vn>
2026-04-03 17:13:29 +0700

    bỏ thư mục iam

--- src/app/(admin)/admin/core/permissions/page.tsx
+++ src/app/(admin)/admin/core/permissions/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { Metadata } from "next";
-import AdminPermissions from "@/components/Features/Core/Iam/Permissions/Admin/AdminPermissions";
+import AdminPermissions from "@/components/Features/Core/Permissions/Admin/AdminPermissions";
 import PageMeta from "@/components/UI/Navigation/PageMeta";
 
 export const metadata: Metadata = {
